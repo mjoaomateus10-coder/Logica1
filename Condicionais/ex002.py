7f9b8db7be604058ec8a534a67273d66a05c552d
João Mateus <mjoaomateus10@gmail.com>
2026-05-06 19:09:42 -0300

    feat: adiciona primeiros desafios de lógica condicional
    Implementação de lógica de tomada de decisão usando if/else. Inclui os arquivos de radar, trabalho terminado e ajuda com caixas.

---
+++ Condicionais/ex002.py
@@ -0,0 +1,13 @@
+# Condicionais - if, elif e else
+
+'''
+Eae João, bora dar uma saida hoje?
+Se eu terminar meu trabalho aqui eu consigo.
+
+'''
+
+trabalho_terminado = False
+if trabalho_terminado == True:
+    print('Bora!')
+else:
+    print('Desculpe, mais ainda não terminei o meu serviço')
---
+++ Condicionais/ex003.py
@@ -0,0 +1,12 @@
+'''
+Eae, Você consegue me ajudar a mover essas caixas lá para fora hoje a tarde?
+
+Se eu estiver livre, Sim. Mas se não der pede meu para o meu irmão te ajudar.
+
+'''
+estou_livre = False
+
+if estou_livre == True:
+    print('Estou livre sim, Irei te ajudar.')
+else:
+    print('Não estarei livre, pedi para o meu irmão te ajudar.')
