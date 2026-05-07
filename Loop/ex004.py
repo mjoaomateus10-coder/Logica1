f5a4c89cfb12f3d6d525c75116a1c250f90daf05
João Mateus <mjoaomateus10@gmail.com>
2026-05-07 20:37:43 -0300

    teste

--- Loop/ex004.py
+++ Loop/ex004.py
@@ -1,3 +1,9 @@
-# Teste
+# Laços de repetição
 
+for item in range(1,20,3):
+    print(item)
 
+'''
+for item in coleção
+    # comandos
+'''
--- Loop/ex005.py
+++ Loop/ex005.py
@@ -1,1 +1,9 @@
-# Teste de laço de repetição
+# Lista de nomes
+
+nomes = ['joão','amanda','rafael','carol']
+dados = [1, 'joao',True,2.5]
+for  nome in nomes:
+    print(nome)
+
+for dado in dados:
+    print(dado)
