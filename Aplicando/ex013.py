ce4e7f8bffc6805f721ed525c6451b42a36578f6
João Mateus <mjoaomateus10@gmail.com>
2026-05-08 23:20:39 -0300

    Contador

---
+++ Aplicando/ex013.py
@@ -0,0 +1,12 @@
+# Contador
+
+contador = 10
+
+while contador >= 0:
+    if contador == 5:
+        print("Metade do caminho!")
+    elif contador == 0:
+        print("🚀 Lançamento!")
+    else:
+        print(contador)
+    contador -= 1
